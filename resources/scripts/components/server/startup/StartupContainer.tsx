12549c9d11bcc13ea00ef5de854c50f52cde77d3
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2024-09-04 13:27:31 +0530

    Update StartupContainer.tsx

--- resources/scripts/components/server/startup/StartupContainer.tsx
+++ resources/scripts/components/server/startup/StartupContainer.tsx
@@ -9,8 +9,6 @@ import { ServerError } from '@/components/elements/ScreenBlock';
 import { httpErrorToHuman } from '@/api/http';
 import { ServerContext } from '@/state/server';
 import { useDeepCompareEffect } from '@/plugins/useDeepCompareEffect';
-import Select from '@/components/elements/Select';
-import isEqual from 'react-fast-compare';
 import Input from '@/components/elements/Input';
 import setSelectedDockerImage from '@/api/server/setSelectedDockerImage';
 import InputSpinner from '@/components/elements/InputSpinner';
@@ -43,11 +41,9 @@ const StartupContainer = () => {
             .includes(variables.dockerImage.toLowerCase());
 
     useEffect(() => {
-        // Since we're passing in initial data this will not trigger on mount automatically. We
-        // want to always fetch fresh information from the API however when we're loading the startup
-        // information.
+        // Fetch fresh information from the API
         mutate();
-    }, []);
+    }, [mutate]);
 
     useDeepCompareEffect(() => {
         if (!data) return;
@@ -71,9 +67,9 @@ const StartupContainer = () => {
                     console.error(error);
                     clearAndAddHttpError({ key: 'startup:image', error });
                 })
-                .then(() => setLoading(false));
+                .finally(() => setLoading(false));
         },
-        [uuid]
+        [uuid, clearFlashes, clearAndAddHttpError, setServerFromState]
     );
 
     return !data ? (
@@ -84,48 +80,26 @@ const StartupContainer = () => {
         )
     ) : (
         <ServerContentBlock title={'Startup Settings'} showFlashKey={'startup:image'}>
-            <div css={tw`md:flex`}>
-                <TitledGreyBox title={'Startup Command'} css={tw`flex-1`}>
-                    <div css={tw`px-1 py-2`}>
-                        <p css={tw`font-mono bg-neutral-900 rounded py-2 px-4`}>{data.invocation}</p>
+            <div css={twmd:flex}>
+                <TitledGreyBox title={'Startup Command'} css={twflex-1}>
+                    <div css={twpx-1 py-2}>
+                        <p css={twfont-mono bg-neutral-900 rounded py-2 px-4}>{data.invocation}</p>
                     </div>
                 </TitledGreyBox>
-                <TitledGreyBox title={'Docker Image'} css={tw`flex-1 lg:flex-none lg:w-1/3 mt-8 md:mt-0 md:ml-10`}>
-                    {Object.keys(data.dockerImages).length > 1 && !isCustomImage ? (
-                        <>
-                            <InputSpinner visible={loading}>
-                                <Select
-                                    disabled={Object.keys(data.dockerImages).length < 2}
-                                    onChange={updateSelectedDockerImage}
-                                    defaultValue={variables.dockerImage}
-                                >
-                                    {Object.keys(data.dockerImages).map((key) => (
-                                        <option key={data.dockerImages[key]} value={data.dockerImages[key]}>
-                                            {key}
-                                        </option>
-                                    ))}
-                                </Select>
-                            </InputSpinner>
-                            <p css={tw`text-xs text-neutral-300 mt-2`}>
-                                This is an advanced feature allowing you to select a Docker image to use when running
-                                this server instance.
-                            </p>
-                        </>
-                    ) : (
-                        <>
-                            <Input disabled readOnly value={variables.dockerImage} />
-                            {isCustomImage && (
-                                <p css={tw`text-xs text-neutral-300 mt-2`}>
-                                    This {"server's"} Docker image has been manually set by an administrator and cannot
-                                    be changed through this UI.
-                                </p>
-                            )}
-                        </>
+                <TitledGreyBox title={'Docker Image'} css={twflex-1 lg:flex-none lg:w-1/3 mt-8 md:mt-0 md:ml-10}>
+                    <p css={twtext-xs text-neutral-300 mt-2}>
+                        Our Images are private :D
+                    </p>
+                    {isCustomImage && (
+                        <p css={twtext-xs text-neutral-300 mt-2}>
+                            This {"server's"} Docker image has been manually set by an administrator and cannot
+                            be changed through this UI.
+                        </p>
                     )}
                 </TitledGreyBox>
             </div>
-            <h3 css={tw`mt-8 mb-2 text-2xl`}>Variables</h3>
-            <div css={tw`grid gap-8 md:grid-cols-2`}>
+            <h3 css={twmt-8 mb-2 text-2xl}>Variables</h3>
+            <div css={twgrid gap-8 md:grid-cols-2}>
                 {data.variables.map((variable) => (
                     <VariableBox key={variable.envVariable} variable={variable} />
                 ))}
@@ -135,3 +109,5 @@ const StartupContainer = () => {
 };
 
 export default StartupContainer;
+
+
